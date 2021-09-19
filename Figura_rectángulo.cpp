57f7cc37a43c48c925122b690b66ef9342e19cf4
Kevin <andrestorrecilla.14@gmail.com>
2021-09-19 13:49:16 -0500

    Punto hallar área del rectángulo terminado

---
+++ Figura_rectángulo.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <wchar.h>
+#include <locale.h>
+
+using namespace std;
+
+int main(){
+	
+	setlocale(LC_ALL, "");
+	
+	int medida;
+	float base;
+	float altura;
+	
+	do {
+		cout << "Digite la medida que va a usar 1(m), 2(cm): " << endl;
+		cin >> medida;
+		
+		if (medida == 1){
+			cout << "Digite la base del rectángulo en metros: " << endl;
+			cin >> base;
+			cout << "Digite la altura del rectángulo en metros: " << endl;
+			cin >> altura;
+		}
+		
+		if (medida == 2){
+			cout << "Digite la base del rectángulo en centímetros: " << endl;
+			cin >> base;
+			cout << "Digite la altura del rectángulo en centímetros: " << endl;
+			cin >> altura;
+		}
+	
+	}while (medida != 1 & medida != 2);
+	
+	cout << "El área del rectángulo es: " << base * altura << endl;
+	
+	system("pause");
+}
