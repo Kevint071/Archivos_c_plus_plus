da4acd110d633f545f2e37222d272d6dc78cd9a1
Kevin <andrestorrecilla.14@gmail.com>
2021-09-20 12:30:14 -0500

    Punto 1 terminado deifinitivamente

--- Figura_rectangulo.cpp
+++ Figura_rectangulo.cpp
@@ -16,24 +16,54 @@ int main(){
 	do {
 		cout << "Digite la medida que va a usar 1(m), 2(cm): " << endl;
 		cin >> medida;
-		
+					
 		if (medida == 1){
+			do{
 			cout << "Digite la base del rectángulo en metros: " << endl;
 			cin >> base;
+			if(base <= 0){
+			cout << "El número no es válido" << endl;
+			}
+			} while(base <= 0);
+			do{
 			cout << "Digite la altura del rectángulo en metros: " << endl;
 			cin >> altura;
-		}
+			if(altura <= 0){
+			cout << "El número de la altura no es válido" << endl;
+			}
+			} while(altura <= 0);
+		}	
+		
 		
 		if (medida == 2){
-			cout << "Digite la base del rectángulo en centï¿½metros: " << endl;
+			do{
+			cout << "Digite la base del rectángulo en centímetros: " << endl;
 			cin >> base;
-			cout << "Digite la altura del rectángulo en centï¿½metros: " << endl;
+			if(base <= 0){
+			cout << "El número no es válido" << endl;
+			}
+			} while(base <= 0);
+			do{
+			cout << "Digite la altura del rectángulo en centímetros: " << endl;
 			cin >> altura;
+			if(altura <= 0){
+			cout << "El número de la base no es válido" << endl;
+			}
+			} while(altura <= 0);
+		}	
+		
+		
+				
+		else{
+			cout << "El número ingresado no es válido" << endl;
 		}
 	
 	}while (medida != 1 & medida != 2);
 	
-	cout << "El ï¿½rea del rectï¿½ngulo es: " << base * altura << endl;
+	
+	
+	cout << "El área del rectángulo es: " << base * altura << endl;
 	
 	system("pause");
+	return 0;
 }
